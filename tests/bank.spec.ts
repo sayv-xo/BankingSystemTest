92095a39b63dabc8acb9464ad6728a90d9157487
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-09-02 01:28:35 +0000

    assert banace and delete customer

--- tests/bank.spec.ts
+++ tests/bank.spec.ts
@@ -4,8 +4,8 @@ const firstName = 'Lord';
 const lastName = 'Voldermort';
 const postCode = 'E42069';
 const fullName = `${firstName} ${lastName}`;
-
-
+const depAmt = 10000;
+const withAmt = 0.2 * depAmt;
 
 test.use({
     browserName: 'chromium',
@@ -52,6 +52,35 @@ async function assertCustomerInTable(page: Page) {
 async function customerLogin(page:Page) {
     await page.click('[ng-click="home()"]');
     await page.click('[ng-click="customer()"]')
+    await page.selectOption('select#userSelect', { label: `${firstName} ${lastName}` });
+    await page.click('button:has-text("Login")');
+}
+
+async function makeDeposit(page:Page) {
+    await page.locator('[ng-click="deposit()"]').click();
+    await page.locator('[ng-model="amount"]').fill(depAmt.toString())
+    await page.getByRole('button', {name: 'Deposit'}).nth(1).click();
+}
+
+async function makeWithdrawal(page:Page) {
+    await page.locator('[ng-click="withdrawl()"]').click();
+    await page.waitForTimeout(2000);
+    await page.locator('[ng-model="amount"]').fill(withAmt.toString());
+    await page.click('button[type="submit"]');
+}
+
+async function assertCorrectBalance(page:Page, expectedBalance:string) {
+    const balanceLocator = page.locator('.center').locator('strong').nth(1);
+    await expect(balanceLocator).toHaveText(expectedBalance);
+}
+
+async function deleteCustomer(page:Page) {
+    await page.click('[ng-click="byebye()"]')
+    await page.click('[ng-click="home()"]');
+    await managerLogin(page);
+    await page.locator('[ng-click="showCust()"]').click();
+    await page.locator('[ng-model="searchCustomer"]').fill(`${firstName}`);
+    await page.click('[ng-click="deleteCust(cust)"]');
 }
 
 test('Open page', async ({ page }) => {
@@ -61,4 +90,8 @@ test('Open page', async ({ page }) => {
     await openCustAccount(page);
     await assertCustomerInTable(page);
     await customerLogin(page);
+    await makeDeposit(page);
+    await makeWithdrawal(page);
+    await assertCorrectBalance(page, '8000');
+    await deleteCustomer(page);
 })
